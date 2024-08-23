37bbad48af29f50956fa9091ea7717e298cc03a3
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2024-08-23 22:53:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,4 @@ sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.
 sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
 sed -i '$a src-git pptp https://github.com/qzyuebing/luci-app-pptp-server' feeds.conf.default
 sed -i '$a src-git YSFClients https://github.com/qzyuebing/YSFClients' feeds.conf.default
+sed -i '$a src-git mmdvm-feeds https://github.com/qzyuebing/mmdvm-feeds' feeds.conf.default
